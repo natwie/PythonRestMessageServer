8e7d16722b32e3fdaf84c7d63fc943216c83a529
Natwie <natalia.wieczorek@gmail.com>
2016-09-10 11:50:22 +0200

    Few functions updated

--- make_some_requests.sh
+++ make_some_requests.sh
@@ -2,12 +2,28 @@
 
 
 echo "Creating 3 simple message"
-curl http://localhost:5000/api/v1.0/create_message/ -H "Content-Type: application/json" -X POST -d '{"username":"sniku", "message": "Hey hey"}' -v
-curl http://localhost:5000/api/v1.0/create_message/ -H "Content-Type: application/json" -X POST -d '{"username":"nupek", "message": "Mehu"}' -v
-curl http://localhost:5000/api/v1.0/create_message/ -H "Content-Type: application/json" -X POST -d '{"username":"sniku", "message": "Jaha"}' -v
+curl http://localhost:5000/api/v1.0/message/create/ -H "Content-Type: application/json" -X POST -d '{"sender":"sniku", "recipient":"nupek", "message": "Hey hey"}' -v
+curl http://localhost:5000/api/v1.0/message/create/ -H "Content-Type: application/json" -X POST -d '{"sender":"nupek", "recipient":"nupek", "message": "Hey hey"}' -v
+curl http://localhost:5000/api/v1.0/message/create/ -H "Content-Type: application/json" -X POST -d '{"sender":"sniku", "recipient":"nupek", "message": "Hey hey"}' -v
+curl http://localhost:5000/api/v1.0/message/create/ -H "Content-Type: application/json" -X POST -d '{"sender":"sniku", "recipient":"nupek", "message": "Hey hey"}' -v
+curl http://localhost:5000/api/v1.0/message/create/ -H "Content-Type: application/json" -X POST -d '{"sender":"sniku", "recipient":"nupek", "message": "Hey hey"}' -v
+curl http://localhost:5000/api/v1.0/message/create/ -H "Content-Type: application/json" -X POST -d '{"sender":"sniku", "recipient":"nupek", "message": "Hey hey"}' -v
+curl http://localhost:5000/api/v1.0/message/create/ -H "Content-Type: application/json" -X POST -d '{"sender":"sniku", "recipient":"nupek", "message": "Hey hey"}' -v
+curl http://localhost:5000/api/v1.0/message/create/ -H "Content-Type: application/json" -X POST -d '{"sender":"sniku", "recipient":"nupek", "message": "Hey hey"}' -v
+curl http://localhost:5000/api/v1.0/message/create/ -H "Content-Type: application/json" -X POST -d '{"sender":"sniku", "recipient":"nupek", "message": "Hey hey"}' -v
 
 echo "Deleting second message"
-curl http://localhost:5000/api/v1.0/delete_message/2/ -X DELETE  -v
+curl http://localhost:5000/api/v1.0/message/delete/2/ -X DELETE  -v
+
+echo "Get unread messages"
+curl http://localhost:5000/api/v1.0/messages/unread/ -X PUT  -v
+
+
+echo "Get all messages in range"
+curl http://localhost:5000/api/v1.0/messages/range/3/4/ -X PUT -v
+
+echo "Deleting messages in range"
+curl http://localhost:5000/api/v1.0/messages/delete/range/6/7/ -X DELETE  -v
 
 echo "Get all messages"
-curl http://localhost:5000/api/v1.0/get_messages/  -v
+curl http://localhost:5000/api/v1.0/messages/ -X PUT -v
--- src/message_server.py
+++ src/message_server.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 from flask import Flask, jsonify, request, abort
 from flask_sqlalchemy import SQLAlchemy
-
 from .validators import json_params_exist
 
 app = Flask(__name__)
@@ -16,13 +15,15 @@ db = SQLAlchemy(app)
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=False)
+    sender = db.Column(db.String(80), unique=False)
+    recipient = db.Column(db.String(80), unique=False)
     message = db.Column(db.Text(), unique=False)
     created_date = db.Column(db.DateTime(), unique=False)
     is_read = db.Column(db.Boolean, unique=False, default=False)
 
-    def __init__(self, username, message):
-        self.username = username
+    def __init__(self, sender, recipient, message):
+        self.sender = sender
+        self.recipient = recipient
         self.message = message
         self.created_date = datetime.utcnow()
         self.is_read = False
@@ -30,31 +31,31 @@ class Message(db.Model):
     def serialize(self):
         return {
             'id': self.id,
-            'username': self.username,
+            'sender': self.sender,
+            'recipient': self.recipient,
             'message': self.message,
             'created_date': self.created_date,
             'is_read':self.is_read,
         }
 
     def __repr__(self):
-        return '<Message {0} {1}>'.format(self.username, self.message)
+        return '<Message {0} {1} {2}>'.format(self.sender, self.recipient, self.message)
 
 
-@app.route("/api/v1.0/create_message/", methods=['POST'])
+@app.route("/api/v1.0/message/create/", methods=['POST'])
 def crete_message():
-
-    valid, message = json_params_exist(request, ['username', 'message'])
+    valid, message = json_params_exist(request, ['sender', 'recipient', 'message'])
     if not valid:
         abort(400, {'message': message})
 
-    m = Message(request.json['username'], request.json['message'])
+    m = Message(request.json['sender'], request.json['recipient'], request.json['message'])
     db.session.add(m)
     db.session.commit()
 
     return jsonify({"created": True, 'id': m.id})
 
 
-@app.route("/api/v1.0/delete_message/<int:id>/", methods=['DELETE'])
+@app.route("/api/v1.0/message/delete/<int:id>/", methods=['DELETE'])
 def delete_message(id):
     m = Message.query.filter_by(id=id).first()
     if m:
@@ -65,14 +66,40 @@ def delete_message(id):
     abort(404, "Message with id {} doesn't exist.".format(id))
 
 
-@app.route("/api/v1.0/get_messages/")
+@app.route("/api/v1.0/messages/delete/range/<int:start>/<int:end>/", methods=['DELETE'])
+def delete_messages_in_range(start, end):
+    messages = db.session.query(Message).filter(Message.id >= start)\
+                                        .filter(Message.id <= end)\
+                                        .delete()
+    db.session.commit()
+    return jsonify({"deleted": messages})
+
+
+@app.route("/api/v1.0/messages/", methods=['PUT'])
 def get_messages():
     messages = Message.query.all()
-    for m in messages:
-        m.is_read = True
-    return jsonify(json_list=[m.serialize() for m in messages])
+    db.session.query(Message).update({Message.is_read: True})
+    db.session.commit()
 
-@app.route("/api/v1.0/get_unread_messages/")
+    return jsonify(messages=[m.serialize() for m in messages])
+
+
+@app.route("/api/v1.0/messages/range/<int:start>/<int:end>/", methods=['PUT'])
+def get_messages_in_range(start, end):
+    messages = Message.query.filter(Message.id >= start)\
+                            .filter(Message.id <= end)\
+                            .order_by(Message.created_date)\
+                            .update({Message.is_read: True})
+    db.session.commit()
+
+    return jsonify(messages=[m.serialize() for m in messages])
+
+
+@app.route("/api/v1.0/messages/unread/", methods=['PUT'])
 def get_unread_messages():
-    messages = Message.query.filter_by(is_read = False)
-    return jsonify(json_list=[m.serialize() for m in messages])
+    messages = db.session.query(Message).filter(Message.is_read == False)
+    underad_messages = [m.serialize() for m in messages]
+    messages.update({Message.is_read: True})
+    db.session.commit()
+
+    return jsonify(messages=underad_messages)
